3fd9ceed2ca4c733af8b060f827e14bf4b41bb06
Abhirath Dubey <abhirathdubey2804@gmail.com>
2024-03-10 22:37:38 +0530

    session update

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(session({
   secret: 'r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#',
   resave: false,
   saveUninitialized: true,
-  cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+  // cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
